06ba56d7fc175e4d61204102760f6cdfdd6a2e1c
Rynnoceros <jordansoubrier@hotmail.fr>
2019-04-24 18:46:27 +0200

    Working on step 4

--- Etape 4/TimeManager.cpp
+++ Etape 4/TimeManager.cpp
@@ -11,22 +11,23 @@ TimeManager::~TimeManager() {
 void TimeManager::Start() {
     _lastStart = std::chrono::system_clock::now();
     _lastUpdate = std::chrono::system_clock::time_point::min();
-    _previousUpdate = std::chrono::system_clock::time_point::min();
+    _elapsedTime = _lastStart - _lastStart;
 }
 
 void TimeManager::Update() {
-    std::time_t previous = std::chrono::system_clock::to_time_t(_previousUpdate);
+    if (_lastUpdate > std::chrono::system_clock::time_point::min()) {
+        _elapsedTime = std::chrono::system_clock::now() - _lastUpdate;
+    }
+
     std::time_t last = std::chrono::system_clock::to_time_t(_lastUpdate);
 
-    std::cout << " Update _previousUpdate: " << std::ctime(&previous) << std::endl;
     std::cout << " Update _lastUpdate: " << std::ctime(&last) << std::endl;
-    _previousUpdate = _lastUpdate;
     _lastUpdate = std::chrono::system_clock::now();
+    std::cout << " Update _lastUpdate: " << std::ctime(&last) << std::endl;
 }
 
 unsigned int TimeManager::GetElapsedTime() const {
-    std::chrono::duration<double> elapsedTime = _lastUpdate - _previousUpdate;
-    return (elapsedTime.count() * 1000);
+    return (_elapsedTime.count() * 1000);
 }
 
 unsigned int TimeManager::GetStartedTime() const {
