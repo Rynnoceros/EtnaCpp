96c94bb9f24c3856e994ba8ff493bd7f31fe8bc8
Rynnoceros <jordansoubrier@hotmail.fr>
2019-04-28 23:00:43 +0200

    Working on step 5

--- Etape 5/MonsterFactory.cpp
+++ Etape 5/MonsterFactory.cpp
@@ -11,9 +11,9 @@ MonsterFactory::~MonsterFactory() {
 }
 
 Character* MonsterFactory::Create(const std::string& toCreate) {
-    if (toCreate.compare("SprinterMonster")) {
+    if (toCreate.compare("SprinterMonster") == 0) {
         return new SprinterMonster();
-    } else if (toCreate.compare("ZombieMonster")) {
+    } else if (toCreate.compare("ZombieMonster") == 0) {
         return new ZombieMonster();
     } else {
         return NULL;
